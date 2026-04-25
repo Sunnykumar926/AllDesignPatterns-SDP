88d319ac82b404de83ffc20a60ebb87226a46854
Sunnykumar926 <118978144+Sunnykumar926@users.noreply.github.com>
2026-04-25 16:09:10 +0530

    added more handler

--- corDP/Chain_of_responsibility.java
+++ corDP/Chain_of_responsibility.java
@@ -37,10 +37,95 @@ class ThousandHandler extends MoneyHandler{
     }
 }
 
+class FiveHundredHandler extends MoneyHandler{
+    private int numNotes;
+    public FiveHundredHandler(int notes){
+        this.numNotes = notes;
+    }
+
+    @Override
+    public void dispense(int amount) {
+        int reqNotes = amount / 500;
+        if(reqNotes > numNotes){
+            reqNotes = numNotes;
+            numNotes = 0;
+        }else{
+            numNotes -= reqNotes;
+        }
+
+        if(reqNotes > 0) System.out.println("Dispensing amount "+reqNotes+ " x ₹500 notes.");
+
+        int remainingAmount = amount - reqNotes*500;
+        if(remainingAmount>0){
+            if(nextHandler!=null) nextHandler.dispense(remainingAmount);
+            else System.out.println("Remaning amount of "+ remainingAmount+" Cann't be fullfilled (Insufficient fund in ATM)");
+        }
+    }
+}
+
+class HundredHandler extends MoneyHandler{
+    private int numNotes;
+    public HundredHandler(int notes){
+        this.numNotes = notes;
+    }
+
+    @Override
+    public void dispense(int amount) {
+        int reqNotes = amount / 100;
+        if(reqNotes > numNotes){
+            reqNotes = numNotes;
+            numNotes = 0;
+        }else{
+            numNotes -= reqNotes;
+        }
+
+        if(reqNotes > 0) System.out.println("Dispensing amount "+reqNotes+ " x ₹100 notes.");
+
+        int remainingAmount = amount - reqNotes*100;
+        if(remainingAmount>0){
+            if(nextHandler!=null) nextHandler.dispense(remainingAmount);
+            else System.out.println("Remaning amount of "+ remainingAmount+" Cann't be fullfilled (Insufficient fund in ATM)");
+        }
+    }
+}
+
+class FiftyHandler extends MoneyHandler{
+    private int numNotes;
+    public FiftyHandler(int notes){
+        this.numNotes = notes;
+    }
+
+    @Override
+    public void dispense(int amount) {
+        int reqNotes = amount / 50;
+        if(reqNotes > numNotes){
+            reqNotes = numNotes;
+            numNotes = 0;
+        }else{
+            numNotes -= reqNotes;
+        }
+
+        if(reqNotes > 0) System.out.println("Dispensing amount "+reqNotes+ " x ₹50 notes.");
+
+        int remainingAmount = amount - reqNotes*50;
+        if(remainingAmount>0){
+            if(nextHandler!=null) nextHandler.dispense(remainingAmount);
+            else System.out.println("Remaning amount of "+ remainingAmount+" Cann't be fullfilled (Insufficient fund in ATM)");
+        }
+    }
+}
+
 
 public class Chain_of_responsibility{
     public static void main(String[] args) {
         MoneyHandler thousandHandler = new ThousandHandler(6);
-        thousandHandler.dispense(4500);
+        MoneyHandler fiveHunderedHandler = new FiveHundredHandler(4);
+        // MoneyHandler hundredHandler = new HundredHandler(3);
+        MoneyHandler fiftyHandler = new FiftyHandler(7);
+
+        thousandHandler.setNextHandler(fiveHunderedHandler);
+        // fiveHunderedHandler.setNextHandler(hundredHandler);
+        fiveHunderedHandler.setNextHandler(fiftyHandler);
+        thousandHandler.dispense(4750);
     }
 }
